0e814c65f6e42d4b69e7bfab9a5f80bd38ea116e
久野泰行 <>
2021-07-09 07:19:38 +0900

    redirectの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,6 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
-    # 以下のメソッドを追加
-    redirect_to action: :index
   end
 
   def edit
@@ -20,15 +18,11 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    # 以下のメソッドを追加
-    redirect_to action: :index
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
-    # 以下のメソッドを追加
-    redirect_to action: :index
   end
 
   private
